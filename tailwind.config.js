a41600b17323be1ec66867cf76b1e05eb6d0930b
krrsh <gmkrishnaa63@gmail.com>
2024-08-09 16:02:49 +0530

    added all files in campuz

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily : {
+      ubuntu : ["Ubuntu", "sans-serif"],
+      kanit : ["Kanit", "sans-serif"],
+      bebas : ["Bebas Neue", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
